f9794868bc3053f3d34e304045af8b939f96c47f
EC2 Default User <ec2-user@ip-10-14-135-9.eu-west-1.compute.internal>
2026-08-24 13:31:45 +0000

    modified Hello World message

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -2,7 +2,7 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO: replace <team-name> with your team's actual name
-        System.out.println("Hello world from the firm's Sprint 1 project skeleton");
+        //
+        System.out.println("Hello world from Group 4's Sprint 1 project skeleton");
     }
 }
