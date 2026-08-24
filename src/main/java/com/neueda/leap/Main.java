0cc6330616930499f3bf03ecdb27b528538caa4d
kbennington <120597594+kbennington@users.noreply.github.com>
2026-08-24 20:33:35 +0000

    Added another message
    Added second hello message

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         //
         System.out.println("Hello world from Group 4's Sprint 1 project skeleton");
+         System.out.println("Hello again");
     }
 }
