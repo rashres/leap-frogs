3f4d953897f089054adb046f7669cc00c875cccd
Rraina12310 <rxr210080@utdallas.edu>
2026-08-25 14:41:14 +0000

    added farewell

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //
         System.out.println("Hello world from Group 4's Sprint 1 project skeleton");
-         System.out.println("Hello again : )");
+        System.out.println("Hello again : )");
+        System.out.println("Farewell! May your code compile on the first try!");
     }
 }
