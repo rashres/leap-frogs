fa6c0ac7d21a3d90406c6c37ba71b2ec039f6c65
Jesse Marasigan <jmarasigan253684@gmail.com>
2026-08-25 14:15:35 +0000

    Added random number generator

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -1,8 +1,12 @@
 package com.neueda.leap;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         //
         System.out.println("Hello world from Group 4's Sprint 1 project skeleton");
+        Random r = new Random();
+        int i = r.nextInt(100);
+        System.out.println("Today's lucky number is " + i);
     }
 }
