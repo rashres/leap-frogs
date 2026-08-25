536110a86595401e123e43eb63ea6f3aa7478cda
kbennington <120597594+kbennington@users.noreply.github.com>
2026-08-25 14:16:21 +0000

    added greeting
    Added user input functionality to greet by name.

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -5,5 +5,15 @@ public class Main {
         //
         System.out.println("Hello world from Group 4's Sprint 1 project skeleton");
          System.out.println("Hello again : )");
+
+        // greet by name
+       
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String userinput= scanner.nextLine();
+        System.out.println("You entered" + userinput);
+        scanner.close;
     }
+
+    
 }
