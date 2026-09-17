f192ce9bc93338507c71b19048ee38ca7a81452c
kbennington <k92ahmed@gmail.com>
2026-09-17 21:08:36 +0000

    refactor: getter add

--- src/main/java/com/neueda/leap/Exchange.java
+++ src/main/java/com/neueda/leap/Exchange.java
@@ -1,5 +1,15 @@
 package com.neueda.leap;
 
 public class Exchange {
-    
+    private int  exchange_id;
+
+    public Exchange(int exchange_id) {
+        this.exchange_id = exchange_id;
+    }
+
+    public int getExchangeId() {
+        
+
+        return this.exchange_id;
+}
 }
