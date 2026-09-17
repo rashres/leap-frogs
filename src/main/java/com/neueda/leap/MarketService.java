9c02ec879311b8edcb18bbbdb182abbbbf23f51d
Jesse Marasigan <jmarasigan253684@gmail.com>
2026-09-17 21:09:15 +0000

    feat: created MarketService class

---
+++ src/main/java/com/neueda/leap/MarketService.java
@@ -0,0 +1,15 @@
+package com.neueda.leap;
+
+import java.math.BigDecimal;
+
+public class MarketService {
+
+    public MarketService() {
+
+    }
+
+    public BigDecimal getPrice() {
+        BigDecimal price = new BigDecimal(0.00000);
+        return price;
+    }
+}
