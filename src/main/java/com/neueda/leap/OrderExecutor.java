63e9355aeaeff2dee15deba1ece4542592cbe40b
Jesse Marasigan <jmarasigan253684@gmail.com>
2026-09-18 20:24:29 +0000

    feat: process_order calls validator

--- src/main/java/com/neueda/leap/OrderExecutor.java
+++ src/main/java/com/neueda/leap/OrderExecutor.java
@@ -5,8 +5,19 @@ public class OrderExecutor {
     public OrderExecutor() {
     }
 
-    public void process_order(Order order) {
+    public boolean process_order(Order order) {
+        order.setStatus("PENDING");
 
+        if (!OrderValidator.isValidOrder(order)) {
+            order.setStatus("FAILED");
+            return false;
+        }
+
+        //TODO: ExternalService Logic
+
+        Account account = order.getAccount();
+        
+        return true;
     }
 
 }
