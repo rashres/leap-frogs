81be536aae1e9c4e97ea2851c2a6e6792566f202
kbennington <k92ahmed@gmail.com>
2026-09-18 19:12:20 +0000

    refactor: added getters/ setters for name and country

--- src/main/java/com/neueda/leap/Exchange.java
+++ src/main/java/com/neueda/leap/Exchange.java
@@ -1,15 +1,36 @@
 package com.neueda.leap;
 
 public class Exchange {
-    private int  exchange_id;
+    private int exchange_id;
+    private String exchangeName;
+    private String country;
+    
 
-    public Exchange(int exchange_id) {
+    public void setExchangeId(int exchange_id) {
         this.exchange_id = exchange_id;
     }
+    
 
     public int getExchangeId() {
         
-
         return this.exchange_id;
 }
+
+
+    public void setName(String exchangeName){
+        this.exchangeName = exchangeName;
+    }
+
+    public String getName(){
+        return this.exchangeName;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
 }
