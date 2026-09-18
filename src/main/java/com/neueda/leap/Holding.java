b40fcfc0fdc1b994ae669bd9ceec584853bf44e9
Rraina12310 <rxr210080@utdallas.edu>
2026-09-18 12:55:50 +0000

    finished instruments class

--- src/main/java/com/neueda/leap/Holding.java
+++ src/main/java/com/neueda/leap/Holding.java
@@ -1,7 +1,56 @@
 package com.neueda.leap;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
 public class Holding {
+    private int holdingId;
+    private int accountId;
+    private Instrument instrument;
+    private BigDecimal quantity = BigDecimal.ZERO
+    private BigDecimal costbasis = BigDecimal.ZERO.setScale(2);
+    private OffsetDateTime updatedAt =  OffsetDateTime.now();
+
+    public Holding()
+
+    public Holding(int holdingId, int accountId, Instrument instrument, BigDecimal quantity){
+        this.holdingId = holdingId;
+        this.accountId = accountId;
+        this.instrument = instrument;
+        this.quantity = quantity;
+
+    }
+
+    public int hetHolding(){
+        return holdingId;
+    }
+    public void setHoldingId(int holdingId){
+        this.holdingId = holdingId;
+    }
+    public int getAccountId(){
+        return accountId;
+    }
+
+    public void setAccountId(int AccountId){
+        this.accountId = accountId;
+    }
+
+    public Instrument getInstrument(){
+        return instrument;
+    }
+    public void setInstrument(int instrument){
+        this.instrument = instrument;
+    }
+    public BigDecimal getQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setCostBasis(BigDecimal costbasis){
+        this.costbasis = costbasis;
+    }
+
 
     
+
     
 }
--- src/main/java/com/neueda/leap/Instrument.java
+++ src/main/java/com/neueda/leap/Instrument.java
@@ -1,5 +1,56 @@
 package com.neueda.leap;
 
 public class Instrument {
+
+    private int stockId;
+    private String symbol;
+    private String name;
+    private int exchangeId;
+
+    public Instrument(){
+
+    }
+
+    public Instrument(int stockId, String symbol, String name, int exchangeId){
+        this.stockId = stockId;
+        this.symbol = symbol;
+        this.name = name;
+        this.exchangeId = exchangeId;
+
+    }
+
+    public int getStockId(){
+        return stockId;
+    }
+
+    public void setStockId(int stockId){
+        this.stockId = stockId;
+    }
+    public String getSymbol(){
+        return symbol;
+    }
+    public void setSymbol(String symbol){
+        this.symbol = symbol;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getExchangeId(){
+        return exchangeId;
+    }
+    public void setExchangeId(int exchangeId){
+        this.exchangeId = exchangeId;
+    }
+
+    public String displayName() {
+        return symbol + " - " + name;
+    }
+    @Override
+    public String toString() {
+        return "Instrument{stockId = " + stockId + ", symbol = " + symbol + ", name = " + name +", exchangeId = " + exchangeId + "}";
+    }
     
 }
