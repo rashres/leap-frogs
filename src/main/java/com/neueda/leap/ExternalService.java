4af99bc1293e7bcc4140d75672ee42cffd8a029d
kbennington <k92ahmed@gmail.com>
2026-09-18 20:49:46 +0000

    refactor: updated executeTrade method

--- src/main/java/com/neueda/leap/ExternalService.java
+++ src/main/java/com/neueda/leap/ExternalService.java
@@ -1,5 +1,10 @@
 package com.neueda.leap;
 
 public class ExternalService {
-    
+    public void executeTrade(Order order) {
+
+            System.out.println("Getting service...");
+        }
+        // send message confirming the creation of the external service
 }
+
