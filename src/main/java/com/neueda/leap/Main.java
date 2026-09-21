ead0824e4249a2a3233a107ff7d4b9239dd92f6a
Jesse Marasigan <jmarasigan253684@gmail.com>
2026-09-21 21:17:28 +0000

    feat: main sample code

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -38,7 +38,6 @@ public class Main {
         System.out.println("\nOrder 2 Results");
         System.out.println("Holding: " + hold2.getInstrument().getName());
         System.out.println("Quantity: " + hold2.getQuantity());
-
         System.out.println("Orders processed");
     }
 }
